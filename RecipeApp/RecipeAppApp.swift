ff2e4b60b29cde82b0a470943b4637a254647ae6
Alex <120681799+TwinkleFoxy@users.noreply.github.com>
2023-01-03 16:54:21 +0500

    Initial Commit

---
+++ RecipeApp/RecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAppApp.swift
+//  RecipeApp
+//
+//  Created by Алексей Однолько on 03.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
